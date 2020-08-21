b34dfb018cd948235e3c2ca164c4f1a6be33f5ef
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-08-21 13:11:46 -0400

    more stuff

--- beam_runner.py
+++ beam_runner.py
@@ -5,7 +5,7 @@ import subprocess
 
 beam_api_dir = '/Users/michaellah/Documents/GitHub/beam-api'
 docker_dir = '.docker/web-services.yml'
-beam_js_dir = ''
+beam_js_dir = '/Users/michaellah/Documents/GitHub/js-frontend'
 
 def on_press(key):
     # os.system('clear')
@@ -21,40 +21,93 @@ def on_release(key):
         # Stop listener
         return False
 
+def open_beam_api():
+    subprocess.run(['rubymine', beam_api_dir])
+
+def start_js():
+    subprocess.run(['yarn', 'start', 'all'], cwd=beam_js_dir)
+
+
+def open_js():
+    subprocess.run(['rubymine', beam_js_dir])
+
+
 def docker():
-    print("DOWN!")
-    # subprocess.run(f'docker-compose -f {beam_api_dir}{docker_dir} down')
-    # TODO replace the rest with subprocess!
     subprocess.run(['docker-compose', '-f', f'{beam_api_dir}/{docker_dir}', "down"], cwd=beam_api_dir)
-    print("BUILD!!!")
     subprocess.run(['docker-compose', '-f', f'{beam_api_dir}/{docker_dir}', "build"], cwd=beam_api_dir)
-    print("UP!")
     subprocess.run(['docker-compose', '-f', f'{beam_api_dir}/{docker_dir}', "up", "-d"], cwd=beam_api_dir)
 
-    # cd
-    # ~ / Documents / GitHub / beam - api /
-    # docker - compose - f.docker / web - services.yml down
-    # docker - compose - f.docker / web - services.yml
-    # build
-    # docker - compose - f.docker / web - services.yml
-    # up - d
 
+def kata_options():
+    print('1. JS Kata')
+    print('2. Ruby Kata')
+    print('3. Marketing Kata')
+    kata_option = input()
+    if option == '1':
+        repo_options()
+    elif option == '2':
+        pass
+    elif option == '3':
+        pass
+    elif option == '4':
+        docker()
+    elif option == '5':
+        pass
+
+
+def repo_options():
+    print('1. beam-api')
+    print('2. js-frontend')
+    print('3. auth-server')
+    print('4. mobile app')
+    print('5. Katas')
+    print('6. Perks server')
+    print('7. PETL')
+    print('8. beam-sftp-dev')
+    repo_option = input()
+
+    if repo_option == '1':
+        pass
+    elif repo_option == '2':
+        open_js()
+    elif repo_option == '3':
+        pass
+    elif repo_option == '4':
+        pass
+    elif repo_option == '5':
+        kata_options
+    elif repo_option == '6':
+        pass
+    elif repo_option == '7':
+        pass
+    elif repo_option == '8':
+        pass
+
+
+    # print('5. JS kata')
+    # print('6. Ruby kata')
 def print_options():
-    os.system('ls')
-    print('1. Full environment start')
+    print('1. open repo')
     print('2. start beam-api ')
     print('3. start js-frontend')
     print('4. start docker')
-    print('5. open repo')
+    print('5. Full environment start')
     print('>> ', end='')
 
 
 if __name__ == '__main__':
     print_options()
-    print(beam_api_dir)
     option = input()
-    if option == '4':
+    if option == '1':
+        repo_options()
+    elif option == '2':
+        pass
+    elif option == '3':
+        start_js()
+    elif option == '4':
         docker()
+    elif option == '5':
+        pass
 
     # with keyboard.Listener(
     #         on_press=on_press,
