71ea07169642b579d12b7375e2ecd705c48c44b2
Michael Lah <42075214+MichaelLah@users.noreply.github.com>
2020-08-21 01:03:10 -0400

    blah

--- beam_runner.py
+++ beam_runner.py
@@ -1,8 +1,10 @@
 from pynput import keyboard
 # Press ⌃R to execute it or replace it with your code.
 import os
+import subprocess
 
 beam_api_dir = '~/Documents/GitHub/beam-api/'
+docker_dir = '.docker/web-services.yml'
 beam_js_dir = ''
 
 def on_press(key):
@@ -20,17 +22,25 @@ def on_release(key):
         return False
 
 def docker():
+    print("DOWN!")
+    # subprocess.run(f'docker-compose -f {beam_api_dir}{docker_dir} down')
+    # TODO replace the rest with subprocess!
+    subprocess.run(['docker-compose', '-f', beam_api_dir, docker_dir, 'down'])
+    print("BUILD!")
+    subprocess.run(f'docker-compose -f {beam_api_dir}{docker_dir} build')
+    print("UP!")
+    subprocess.run(f'docker-compose -f {beam_api_dir}{docker_dir} up')
     pass
     # cd
     # ~ / Documents / GitHub / beam - api /
-    # docker - compose - f.docker / web - services.yml
-    # down
+    # docker - compose - f.docker / web - services.yml down
     # docker - compose - f.docker / web - services.yml
     # build
     # docker - compose - f.docker / web - services.yml
     # up - d
 
 def print_options():
+    os.system('ls')
     print('1. Full environment start')
     print('2. start beam-api ')
     print('3. start js-frontend')
@@ -43,6 +53,9 @@ if __name__ == '__main__':
     print_options()
     print(beam_api_dir)
     option = input()
+    if option == '4':
+        docker()
+
     # with keyboard.Listener(
     #         on_press=on_press,
     #         on_release=on_release) as listener:
